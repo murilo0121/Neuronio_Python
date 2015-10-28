d3cff4248bc3f6624479ddfab964116087cb0c87
murilo0121 <murilo0121@gmail.com>
2015-10-28 20:47:32 -0200

    Le a quantidade de dendritos automaticamente

--- main.py
+++ main.py
@@ -47,8 +47,7 @@ class Neuronio():
         if(resultado < 0):
             print('-1')
 
-    def sigmoid(self, x):
-        return 1 / (1 + math.exp(-x))
+
 
     def somatorio(self, amostra):
         soma = 0
@@ -66,37 +65,32 @@ def criaDendridos(quantidadeDentridos):
 
 
 if __name__ == '__main__':
-    print('Insira a quantidade de dentridos, o valor deve ter a mesma quantidade de valores de entrada:')
-    #colocar para ler a quantidade de dentritos automaticamente,
-    quantidadeDeDendritos = int(input(''))
+
+    amostras = [[0.72, 0.82], [0.91, -0.69],
+       [0.46, 0.80],   [0.03, 0.93],
+       [0.12, 0.25],   [0.96, 0.47],
+       [0.8, -0.75],   [0.46, 0.98],
+       [0.66, 0.24],   [0.72, -0.15],
+       [0.35, 0.01],   [-0.16, 0.84],
+       [-0.04, 0.68],  [-0.11, 0.1],
+       [0.31, -0.96],   [0.0, -0.26],
+       [-0.43, -0.65],  [0.57, -0.97],
+       [-0.47, -0.03],  [-0.72, -0.64],
+       [-0.57, 0.15],   [-0.25, -0.43],
+       [0.47, -0.88],   [-0.12, -0.9],
+       [-0.58, 0.62],   [-0.48, 0.05],
+       [-0.79, -0.92],  [-0.42, -0.09],
+       [-0.76, 0.65],   [-0.77, -0.76]]
+
+    saidas = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,]
+
+    quantidadeDeDendritos = len(amostras[0])
     dendritos = Dendrito(criaDendridos(quantidadeDeDendritos))
     neuronio = Neuronio(dendritos, sign)
-    amostras = [[0.1, 0.4, 0.7], [0.3, 0.7, 0.2],
-                [0.6, 0.9, 0.8], [0.5, 0.7, 0.1]]
-    saidas = [1, -1, -1, 1]
+
     neuronio.treinamento(amostras, saidas, 1000, 0.1, -1, sign)
-    amostras = [0.1, 0.4, 0.7]
-    neuronio.start(amostras)
-    amostras = [0.3, 0.7, 0.2]
-    neuronio.start(amostras)
-    amostras = [0.6, 0.9, 0.8]
-    neuronio.start(amostras)
-    amostras = [0.5, 0.7, 0.1]
+    amostras = [1.0, -1.0]
     neuronio.start(amostras)
 
-    amostras2 = [[1,4000,45,1], [1,5000,40,1], [2,3500,40,0],[2,300,39,1],[1,200,18,0],[1,400,50,1],[2,5000,40,1],[2,200,25,0]]
-
-    saidas2 = [1,1,1,0,0,1,1,0]
-
-    testes2 = copy.deepcopy(amostras2)
-
-    print('Insira a quantidade de dentridos, o valor deve ter a mesma quantidade de valores de entrada:')
-    quantidadeDeDendritos = int(input(''))
-    dendritos = Dendrito(criaDendridos(quantidadeDeDendritos))
-
-    neuronio2 = Neuronio(dendritos, sign)
-    neuronio2.treinamento(amostras2, saidas2, 1000, 0.1, -1, sign)
-    teste = [1, 300, 80, 1]
-    neuronio2.start(teste)
 
 
